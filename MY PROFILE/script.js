89d5554f614c5f1fdf08c5affce6b8b3dd4b1597
Despenser01 <152034392+Despenser01@users.noreply.github.com>
2024-02-08 16:41:11 -0800

    Add files via upload

---
+++ MY PROFILE/script.js
@@ -0,0 +1,21 @@
+let header = document.querySelector('header');
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    navbar.classList.toggle('active');
+}
+
+// darkmode
+let darkmode = document.querySelector('#darkmode');
+
+darkmode.onclick = () =>{
+    if(darkmode.classList.contains('fa-moon')){
+    darkmode.classList.replace('fa-moon', 'fa-sun');
+    document.body.classList.add('active');
+    }
+    else{
+        darkmode.classList.replace('fa-sun', 'fa-moon');
+        document.body.classList.remove('active');
+    }
+}
